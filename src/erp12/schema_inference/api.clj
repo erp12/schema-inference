9c52bef1a8235f11071f6ed8a44fa6d492a4949e
erp12 <eddie.pantridge@gmail.com>
2023-07-18 13:25:22 +0100

    Removes concretize function.

--- src/erp12/schema_inference/api.clj
+++ src/erp12/schema_inference/api.clj
@@ -1,28 +1,7 @@
 (ns erp12.schema-inference.api
-  (:require [erp12.schema-inference.impl.algo_w :as algo-w]
-            [erp12.schema-inference.impl.util :as u]))
+  (:require [erp12.schema-inference.impl.algo_w :as algo-w]))
 
 (defn infer-schema
   "Infer the schema of the expression given the environment."
   [ast env]
   (algo-w/infer-schema ast env))
-
-(defn concretize
-  [bindings {:keys [type] :as schema}]
-  (let [schema (if (= type :scheme) (:body schema) schema)]
-    (->> schema
-         (u/substitute bindings)
-         (u/generalize {}))))
-
-
-(comment
-
-  (concretize {'T {:type 'int?}}
-              {:type   :scheme
-               :s-vars ['T]
-               :body   {:type   :=>
-                        :input  {:type     :cat
-                                 :children [{:type :s-var, :sym 'T}]}
-                        :output {:type :s-var, :sym 'T}}})
-
-  )
